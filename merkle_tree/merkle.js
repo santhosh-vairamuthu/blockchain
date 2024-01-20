f82e2a27378ec5481bd6c79352275fa78f906479
santhosh <santhosh22032004@gmail.com>
2024-01-20 11:03:53 +0530

    t2

--- merkle_tree/merkle.js
+++ merkle_tree/merkle.js
@@ -43,42 +43,86 @@ class MerkleTree {
         return this.root;
     }
 
+    findIndex(x,y){
+        let z = x <= y ? x : y;
+        return z/2;
+    }
+
+    findNeighbors(saveHash, z , i){
+        let p = -1;
+        if (z%2 == 0 && z == this.treeData[i].length -1) {
+            saveHash = this.hash(saveHash + saveHash);
+            p=z;
+        }else if (z % 2 == 0) {
+            saveHash = this.hash(saveHash + this.treeData[i][z + 1]);
+            p = z+1;
+        } else {
+            saveHash = this.hash(this.treeData[i][z - 1] + saveHash);
+            p = z-1;
+        }
+        return {saveHash,p};
+    }
+
     validateTransaction(transaction) {
         let saveHash = this.hash(transaction);
-        for (let i = 0; i < this.treeData.length; i++) {
-            let z = -1;
-            for (let j = 0; j < this.treeData[i].length; j++) {
-                if (this.treeData[i][j] == saveHash) {
-                    saveHash = this.treeData[i][j];
-                    z = j;
-                    break;
-                }
-            }
-            if (z == -1) {
-                return "TRANSACTION not found";
-            }
+        let z = -1,p=-1;
 
-            if (z%2 == 0 && z == this.treeData[i].length -1) {
-                saveHash = this.hash(saveHash + saveHash);
-            }else if (z % 2 == 0) {
-                saveHash = this.hash(saveHash + this.treeData[i][z + 1]);
-            } else {
-                saveHash = this.hash(this.treeData[i][z - 1] + saveHash);
+        for (let j = 0; j < this.treeData[0].length; j++) {
+            if (this.treeData[0][j] == saveHash) {
+                z = j;
+                break;
             }
         }
+
+        if (z == -1) {
+            return "TRANSACTION not found";
+        }
+
+        const obj = this.findNeighbors(saveHash,z,0);
+        
+        saveHash = obj.saveHash;
+        p = obj.p;
+
+        let index = this.findIndex(z,p);
+
+
+        for (let i = 1; i < this.treeData.length; i++) {
+
+            let newHashIndex = -1,neighborIndex = -1;
+
+            if (this.treeData[i][index] === saveHash) {
+                newHashIndex = index;
+            } 
+
+            if (newHashIndex == -1) {
+                return "TRANSACTION not found";
+            }
+            
+            const obj2 = this.findNeighbors(saveHash, newHashIndex,i);
+            
+            saveHash = obj2.saveHash;
+            neighborIndex = obj2.p;
+
+            index = this.findIndex(newHashIndex, neighborIndex);
+        }
+
         return saveHash == this.root;
     }
 }
 
 // Example usage
-const transactions = ["Tx1", "Tx2", "Tx3", "Tx4", "Tx5"];
+const transactions = ["Tx1", "Tx2", "Tx3", "Tx4", "Tx5", "Tx6", "Tx7", "Tx8"];
 const merkleTree = new MerkleTree(transactions);
 
 console.log("Merkle Root:", merkleTree.getRoot());
 
 // Validate a transaction
-const transaction1 = "Tx5";
+const transaction1 = "Tx1";
 const transaction2 = "TxInvalid";
 
 console.log(`${transaction1} - ${merkleTree.validateTransaction(transaction1)}`);
 console.log(`${transaction2} - ${merkleTree.validateTransaction(transaction2)}`);
+
+// for(let i = 0; i <merkleTree.treeData.length; i++){
+//     console.log(merkleTree.treeData[i]);
+// }
